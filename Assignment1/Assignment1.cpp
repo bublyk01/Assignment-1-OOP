ac1130fe73a527dddb13fbf156b11414af0e9dc3
Volodya Bublyk <151150647+bublyk01@users.noreply.github.com>
2024-09-11 16:12:12 +0300

    Class declaration

--- Assignment1/Assignment1.cpp
+++ Assignment1/Assignment1.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
 
-int main()
-{
-    std::cout << "Hello World!\n";
-}
+class Airplane {
+private:
+	std::string NumberOfSeats;
+	std::string AvailableSeat;
+	std::string OccupiedSeat;
+	std::string FlightDate;
+	std::string FlightNumber;
+	std::string SeatPrice;
+public:
+	Airplane(std::string seats, std::string free, std::string taken, std::string date, std::string number, std::string price)
+		:NumberOfSeats(seats), AvailableSeat(free), OccupiedSeat(taken), FlightDate(date), FlightNumber(number), SeatPrice(price) {}
+};
+
+class Ticket {
+private:
+	std::string Name;
+	std::string SeatNumber;
+	std::string FlightNumber;
+	std::string Status;
+public:
+	Ticket(std::string name, std::string number, std::string flight, std::string status)
+		:Name(name), SeatNumber(number), FlightNumber(flight), Status(status) {}
+};
+
+//class Reader {
+
+//};
