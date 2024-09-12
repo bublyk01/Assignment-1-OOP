f67cf342addf56a9d5fdae463b3d172d3b92441a
Volodya Bublyk <151150647+bublyk01@users.noreply.github.com>
2024-09-12 16:19:32 +0300

    Getter methods were done

--- Assignment1/Assignment1.cpp
+++ Assignment1/Assignment1.cpp
@@ -1,44 +1,85 @@
 #include <iostream>
+#include <string>
 
 class Airplane {
 private:
-	std::string NumberOfSeats;
-	std::string AvailableSeat;
-	std::string OccupiedSeat;
-	std::string FlightDate;
-	std::string FlightNumber;
-	std::string SeatPrice;
+    std::string NumberOfSeats;
+    std::string AvailableSeat;
+    std::string OccupiedSeat;
+    std::string FlightDate;
+    std::string FlightNumber;
+    std::string SeatPrice;
 public:
-	Airplane(std::string seats, std::string free, std::string taken, std::string date, std::string number, std::string price)
-		:NumberOfSeats(seats), AvailableSeat(free), OccupiedSeat(taken), FlightDate(date), FlightNumber(number), SeatPrice(price) {}
+    Airplane(std::string seats, std::string free, std::string taken, std::string date, std::string number, std::string price)
+        :NumberOfSeats(seats), AvailableSeat(free), OccupiedSeat(taken), FlightDate(date), FlightNumber(number), SeatPrice(price) {}
 
-	friend class Reader; //likely a temporary thing
+    friend class Reader; //likely a temporary thing
 };
 
 class Ticket {
 private:
-	std::string Name;
-	std::string SeatNumber;
-	std::string FlightNumber;
-	std::string Status;
+    std::string Name;
+    std::string SeatNumber;
+    std::string FlightNumber;
+    std::string Status;
 public:
-	Ticket(std::string name, std::string number, std::string flight, std::string status)
-		:Name(name), SeatNumber(number), FlightNumber(flight), Status(status) {}
+    Ticket(std::string name, std::string seat, std::string flight, std::string status)
+        :Name(name), SeatNumber(seat), FlightNumber(flight), Status(status) {}
 
-	friend class Reader; //the same goes here, too
+    friend class Reader; //the same goes here, too
 };
 
 class Reader {
 public:
-	std::string getNumberOfSeats(const Airplane& airplane);
-	std::string getAvailableSeat(const Airplane& airplane);
-	std::string getOccupiedSeat(const Airplane& airplane);
-	std::string getFlightDate(const Airplane& airplane);
-	std::string getFlightNumber(const Airplane& airplane);
-	std::string getSeatPrice(const Airplane& airplane);
+    std::string getNumberOfSeats(const Airplane& airplane) const;
+    std::string getAvailableSeat(const Airplane& airplane) const;
+    std::string getOccupiedSeat(const Airplane& airplane) const;
+    std::string getFlightDate(const Airplane& airplane) const;
+    std::string getFlightNumber(const Airplane& airplane) const;
+    std::string getSeatPrice(const Airplane& airplane) const;
 
-	std::string getName(const Ticket& ticket);
-	std::string getSeatNumber(const Ticket& ticket);
-	std::string getFlightNumebr(const Ticket& ticket);
-	std::string getStatus(const Ticket& ticket);
+    std::string getName(const Ticket& ticket) const;
+    std::string getSeatNumber(const Ticket& ticket) const;
+    std::string getFlightNumber(const Ticket& ticket) const;
+    std::string getStatus(const Ticket& ticket) const;
 };
+
+std::string Reader::getNumberOfSeats(const Airplane& airplane) const {
+    return airplane.NumberOfSeats;
+}
+
+std::string Reader::getAvailableSeat(const Airplane& airplane) const {
+    return airplane.AvailableSeat;
+}
+
+std::string Reader::getOccupiedSeat(const Airplane& airplane) const {
+    return airplane.OccupiedSeat;
+}
+
+std::string Reader::getFlightDate(const Airplane& airplane) const {
+    return airplane.FlightDate;
+}
+
+std::string Reader::getFlightNumber(const Airplane& airplane) const {
+    return airplane.FlightNumber;
+}
+
+std::string Reader::getSeatPrice(const Airplane& airplane) const {
+    return airplane.SeatPrice;
+}
+
+std::string Reader::getName(const Ticket& ticket) const {
+    return ticket.Name;
+}
+
+std::string Reader::getSeatNumber(const Ticket& ticket) const {
+    return ticket.SeatNumber;
+}
+
+std::string Reader::getFlightNumber(const Ticket& ticket) const {
+    return ticket.FlightNumber;
+}
+
+std::string Reader::getStatus(const Ticket& ticket) const {
+    return ticket.Status;
+}
