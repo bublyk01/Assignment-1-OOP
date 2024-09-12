80e2431d5f7aedce6e0d09d41594069dadc3e527
Volodya Bublyk <151150647+bublyk01@users.noreply.github.com>
2024-09-12 15:18:39 +0300

    Declared the Reader class

--- Assignment1/Assignment1.cpp
+++ Assignment1/Assignment1.cpp
@@ -11,6 +11,8 @@ private:
 public:
 	Airplane(std::string seats, std::string free, std::string taken, std::string date, std::string number, std::string price)
 		:NumberOfSeats(seats), AvailableSeat(free), OccupiedSeat(taken), FlightDate(date), FlightNumber(number), SeatPrice(price) {}
+
+	friend class Reader; //likely a temporary thing
 };
 
 class Ticket {
@@ -22,8 +24,21 @@ private:
 public:
 	Ticket(std::string name, std::string number, std::string flight, std::string status)
 		:Name(name), SeatNumber(number), FlightNumber(flight), Status(status) {}
+
+	friend class Reader; //the same goes here, too
 };
 
-//class Reader {
+class Reader {
+public:
+	std::string getNumberOfSeats(const Airplane& airplane);
+	std::string getAvailableSeat(const Airplane& airplane);
+	std::string getOccupiedSeat(const Airplane& airplane);
+	std::string getFlightDate(const Airplane& airplane);
+	std::string getFlightNumber(const Airplane& airplane);
+	std::string getSeatPrice(const Airplane& airplane);
 
-//};
+	std::string getName(const Ticket& ticket);
+	std::string getSeatNumber(const Ticket& ticket);
+	std::string getFlightNumebr(const Ticket& ticket);
+	std::string getStatus(const Ticket& ticket);
+};
